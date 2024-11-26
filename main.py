0ed2c6a9d8d3764c4d5185c66db50db5004ea32a
Jakub <56446320+Niewiaro@users.noreply.github.com>
2024-11-26 19:15:03 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+from transformers import pipeline
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
